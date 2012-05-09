f96236458c9dc6291f2c06f698838ec651b76c8a
Lin Yiming <linym@rd.netease.com>
2012-05-09 18:09:10 +0800

    MOD:rename_project script

--- scripts/rename_project.sh
+++ scripts/rename_project.sh
@@ -1,51 +1,52 @@
-#! /bin/sh
+#!/bin/bash
+#
 #
 
 usage()
 {
-    cat <<-EOF >&2
-
-    Script that help you rename the project from Template to anything you like
-    usage: ./${0##*/}  -d dir < -f originName> -t newName
-    -d     dir          project root directory
-    -f     originName   original project name(default 'Template')
-    -t     newName      new project name
-    -h     N/A          Print this message
-    example:
-    $ $0 -d ./  newProjectName
-    EOF
+    cat << USAGE
+        Script that help you rename the project from Template to anything you like
+        usage: rename_project  -d dir < -f originName> -t newName
+            -d     dir          project root directory
+            -f     originName   original project name(default 'Template')
+            -t     newName      new project name
+            -h     N/A          Print this message
+        example:
+        $ $0 -d ./  -t newProjectName
+USAGE
     exit 0
 }
 
 #default 
 originName="Template"
 
-while getopts h:d:f:t arg
+while getopts h:d:f:t: arg
 do  case $arg in
     h) usage;;
-    d) DIR="$OPTARG"
-    f) originName="$OPTARG";;
-    t) newName="OPTARG";;
+    d) DIR=$OPTARG;;
+    f) originName=$OPTARG;;
+    t) newName=OPTARG;;
     *) usage;;
 esac
 done
 
-[ -d "$DIR" ]|| \
-    {
-        echo "Error: Directory '$DIR' doesn't exist!"
-        usage
-    }
-[ -n "$newName" ] \
-    {
+if [ ! -d "$DIR" ];then
+    echo "Error: Directory '$DIR' doesn't exist!"
+    usage;
+fi
+
+if [ ! -n "$newName" ];then 
     echo "Error: new project name needed!"
-    usage
-    }
+    usage;
+fi
+
 pushd $DIR
-projectFile="$originName.xcodeproj/project.pbxproj"
-[ -f "$projectFile" ] || \ 
-{
+projectFile="$DIR$originName.xcodeproj/project.pbxproj"
+if [ ! -f "$projectFile" ];then
     echo "Error:'$projectFile' doesn't exit or not readable!"
-}
+fi
+
+# replace
 sed 's;$originName;$newName;' $projectFile
 
 # remove xcworkspace and xcuserdata 
@@ -53,9 +54,10 @@ rm -rf $originName".xcodeproj/project.xcworkspace"
 rm -rf $originName".xcodeporj/xcuserdata"
 
 # rename dir
-mv $originName##.xcodeproj $newName##.xcodeproj
-mv $originName##Tests $newName##Tests
+mv $originName".xcodeproj" $newName".xcodeproj"
+mv $originName"Tests" $newName"Tests"
 mv $originName $newName
+
 popd
 
-
+exit 0
